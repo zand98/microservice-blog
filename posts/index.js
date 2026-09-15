24558b3a0a882162c7ba6e0b6c51eddfecc48aa9
zand98 <zandyasin98@gmail.com>
2026-09-15 10:43:49 +0300

    chore: update posts container image to zandyasin and update startup log format

--- posts/index.js
+++ posts/index.js
@@ -41,6 +41,9 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log("v5.5");
+
+  console.log("======================");
+  console.log("========v55==========");
+  console.log("======================");
   console.log("Listening on 4000");
 });
