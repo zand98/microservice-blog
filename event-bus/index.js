c2641187ebb5124923cc8ce7cbfbeac212aff4ed
zand98 <zandyasin98@gmail.com>
2026-09-16 15:01:10 +0300

    feat: add Kubernetes deployment manifests and update inter-service communication to use cluster services

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,9 +14,9 @@ app.post("/events", (req, res) => {
 
   try {
     axios.post("http://posts-clusterip-srv:4000/events", event);
-    // axios.post("http://localhost:4001/events", event);
-    // axios.post("http://localhost:4002/events", event);
-    // axios.post("http://localhost:4003/events", event);
+    axios.post("http://comments-clusterip-srv:4001/events", event);
+    axios.post("http://query-clusterip-srv:4002/events", event);
+    axios.post("http://moderation-clusterip-srv:4003/events", event);
   } catch (error) {}
   res.send({ status: "OK" });
 });
